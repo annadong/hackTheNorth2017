6fe241174eb3b8942331f5a61d1927b00e97fbf9
ANNA DONG <annarhdong@gmail.com>
2017-09-16 16:37:53 -0400

    ready for merge

---
+++ HackTheNorth_TestProject/CreateAccountViewController.swift
@@ -0,0 +1,13 @@
+//
+//  CreateAccountViewController.swift
+//  HackTheNorth_TestProject
+//
+//  Created by rh2liu on 2017-09-16.
+//  Copyright © 2017 Anna Dong. All rights reserved.
+//
+
+import UIKit
+
+class CreateAccountViewController: UIViewController {
+}
+
