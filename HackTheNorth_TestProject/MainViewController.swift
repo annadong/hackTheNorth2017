fbd11cf503fffbfa98b9ec105381ce65e602bed7
lynneatan <lynneatan@gmail.com>
2017-09-16 16:02:06 -0400

    Basic flow for app and sections mocked up.

---
+++ HackTheNorth_TestProject/MainViewController.swift
@@ -0,0 +1,39 @@
+//
+//  mainPage.swift
+//  HackTheNorth_TestProject
+//
+//  Created by Lynnea Tan on 2017-09-16.
+//  Copyright © 2017 Anna Dong. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func FoodTracking(sender: AnyObject) {
+    
+    }
+    
+    @IBAction func Symptoms(sender: AnyObject) {
+    }
+    
+    @IBAction func Prescriptions(sender: AnyObject) {
+    }
+    
+    @IBAction func Data(sender: AnyObject) {
+        
+    }
+}
+
---
+++ HackTheNorth_TestProject/PrescriptionViewController.swift
@@ -0,0 +1,33 @@
+//
+//  PrescriptionViewController.swift
+//  HackTheNorth_TestProject
+//
+//  Created by Lynnea Tan on 2017-09-16.
+//  Copyright © 2017 Anna Dong. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class PrescriptionViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func Search(sender: AnyObject) {
+    }
+    @IBAction func Capture(sender: AnyObject) {
+    }
+    @IBAction func ManualEntry(sender: AnyObject) {
+    }
+    
+}
