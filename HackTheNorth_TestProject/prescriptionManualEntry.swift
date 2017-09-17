d445e5d3da4d521f598208bc020be35858e0cc44
lynneatan <lynneatan@gmail.com>
2017-09-16 21:44:10 -0400

    Cleaned some pages up

---
+++ HackTheNorth_TestProject/prescriptionManualEntry.swift
@@ -0,0 +1,28 @@
+//
+//  prescriptionManualEntry.swift
+//  HackTheNorth_TestProject
+//
+//  Created by Lynnea Tan on 2017-09-16.
+//  Copyright © 2017 Anna Dong. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class prescriptionManualEntryr: UIViewController {
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
